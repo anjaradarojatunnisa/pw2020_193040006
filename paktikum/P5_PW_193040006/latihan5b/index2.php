cc573eb4f3c93181e2fe99c7957dcbcc39c72c1d
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2020-04-17 08:28:46 -0700

    P5_PW_193040006
    tugas P5

---
+++ paktikum/P5_PW_193040006/latihan5b/index2.php
@@ -0,0 +1,52 @@
+<?php
+// koneksi
+$conn = mysqli_connect("localhost","root","", "tugasbesar_193040006");
+
+//ambil data dari tabel mahasiswa
+$result = mysqli_query($conn,"SELECT * FROM elektronik");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<!--link css-->
+<link rel="stylesheet" type="text/css" href="style.css">
+    
+    <title>Halaman admin</title>
+
+</head>
+<body>
+
+<h1> daftar elektronik</h1>
+
+<table border="1" cellpadding="10" cellspacing="0">
+
+<tr>
+            <th>No</th>
+            <th>aksi</th>
+            <th>nama</th>
+            <th>warna</th>
+            <th>stok</th>
+            <th>harga</th>
+            <th>gambar</th>
+        </tr>
+
+        <?php $i = 1; ?>
+        <?php while( $row = mysqli_fetch_assoc($result) ) : ?>
+        <tr>
+            <td> <?= $i; ?> </td>
+            <td>
+                <a href="">ubah</a> |
+                <a href="">hapus</a>
+            </td>
+            <td> <?= $row["nama"]; ?> </td>
+            <td> <?= $row["warna"]; ?> </td>
+            <td> <?= $row["harga"]; ?> </td>
+            <td> <?= $row["stok"]; ?> </td>
+            <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+        </tr>
+        <?php $i++; ?>
+        <?php endwhile; ?>
+    </tible>
+</body>
+</html>
